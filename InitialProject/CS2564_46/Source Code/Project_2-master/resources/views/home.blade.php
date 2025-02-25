31529fcc7d6263fcb6c4a7b9d34c6929c75b3f6f
Supatz317 <supatsorn.su@kkumail.com>
2025-02-25 22:57:42 +0700

    [fix] : extend image not working

--- InitialProject/CS2564_46/Source Code/Project_2-master/resources/views/home.blade.php
+++ InitialProject/CS2564_46/Source Code/Project_2-master/resources/views/home.blade.php
@@ -81,6 +81,13 @@
         z-index:-10;
     }
 
+    .carousel {
+        max-width: 960px;
+        width: 100%;
+        height: 100%;
+        min-width:100vw;
+    }
+
 </style>
 @section('content')
 
