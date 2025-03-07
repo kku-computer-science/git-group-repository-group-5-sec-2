3a956bb40390032b489a2550c64368dc77538a3c
0000nopI <nopparuj.i@kkumail.com>
2025-03-08 01:00:30 +0700

    add tag list for search

--- sourceCode/app/Http/Controllers/AllHighlightController.php
+++ sourceCode/app/Http/Controllers/AllHighlightController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Highlight;
+use App\Models\Tags;
 
 class AllHighlightController extends Controller
 {
     public function index()
     {
         $highlights = Highlight::orderBy('created_at', 'desc')->get();
-        return view('allhighlights.index', compact('highlights'));
+        $tags = Tags::all();
+        return view('allhighlights.index', compact('highlights', 'tags'));
     }
 
 
