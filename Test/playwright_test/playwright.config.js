1bf5e3def8560da5f70b654becb7f5a6033a90af
Supatz317 <supatsorn.su@kkumail.com>
2025-03-11 21:05:51 +0700

    Test/playwright_test/playwright.config.js

--- Test/playwright_test/playwright.config.js
+++ Test/playwright_test/playwright.config.js
@@ -37,17 +37,14 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { 
+        ...devices['Desktop Chrome'] ,
+        deviceScaleFactor: undefined,
+        viewport: null,
+        launchOptions: {
+          args: ['--start-maximized'],
+        },
+      },
     },
 
     /* Test against mobile viewports. */
